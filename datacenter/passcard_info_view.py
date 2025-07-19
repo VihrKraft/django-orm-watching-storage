e5a210a8ed0b1d5a41d98e7822a8d70ad5651382
Vihr_Kraft <sanefedov09@yandex.ru>
2025-07-19 20:27:43 +0700

    Изменил названия переменных

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -13,20 +13,20 @@ def get_duration(visit):
         delta = leaved_time-entry_time
     else:
         delta = time_now-entry_time
-    all_minutes = round(delta.total_seconds()/60)
-    return all_minutes
+    minutes = round(delta.total_seconds()/60)
+    return minutes
 
 
 
-def format_duration(all_minutes):
-    hours = all_minutes // 60
-    minutes = all_minutes-hours*60
-    return f'{hours}ч {minutes}мин'
+def format_duration(minutes):
+    hours = minutes // 60
+    remaining_minutes = minutes-hours*60
+    return f'{hours}ч {remaining_minutes}мин'
 
 
-def is_visit_long(all_minutes):
-    all_hours = all_minutes / 60
-    if all_hours>1:
+def is_visit_long(visit_time, minutes=60):
+    hours = visit_time/minutes
+    if hours>1:
         return True
     else:
         return False
@@ -39,14 +39,14 @@ def passcard_info_view(request, passcode):
     this_passcard_visits = []
 
     for visit in visits:
-        all_minutes = get_duration(visit)
+        minutes = get_duration(visit)
 
-        duration = format_duration(all_minutes)
+        duration = format_duration(minutes)
 
         this_passcard_visits.append({
             'entered_at': visit.entered_at,
             'duration': duration,
-            'is_strange': is_visit_long(all_minutes),
+            'is_strange': is_visit_long(minutes),
         })
 
     context = {

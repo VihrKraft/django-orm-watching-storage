0051cb4dd96dc0eeb29785b7381074bfc8b40408
Vihr_Kraft <sanefedov09@yandex.ru>
2025-07-19 19:55:06 +0700

    Вынес настройку ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ DEBUG = env.bool('SECRET_DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1:8000']
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
